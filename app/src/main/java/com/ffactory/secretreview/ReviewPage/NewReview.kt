f80f67bde2898d7e61297042eb5ccd2bd4c1bed9
cbh1992 <whgg9786@gmail.com>
2023-12-07 19:11:14 +0900

    파이어베이스 연결 추가(테스트 완료)

--- app/src/main/java/com/ffactory/secretreview/ReviewPage/NewReview.kt
+++ app/src/main/java/com/ffactory/secretreview/ReviewPage/NewReview.kt
@@ -1,14 +1,36 @@
 package com.ffactory.secretreview.ReviewPage
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.ffactory.secretreview.databinding.ActivityNewReviewBinding
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 class NewReview : AppCompatActivity() {
     private lateinit var binding : ActivityNewReviewBinding
+    val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding =ActivityNewReviewBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //파이어스토어 테스트구역
+        binding.NewWritten.setOnClickListener {
+            val user = hashMapOf(
+                "first" to "Ada",
+                "last" to "Lovelace",
+                "born" to 1815
+            )
+            db.collection("users")
+                .add(user)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "Error adding document", e)
+                }
+        }
     }
 }
