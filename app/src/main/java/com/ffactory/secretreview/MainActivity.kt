70d916f9d559a012a0defd52a9315907b58bf458
cbh1992 <whgg9786@gmail.com>
2023-12-11 22:56:41 +0900

    리사이클러뷰 공사중...

--- app/src/main/java/com/ffactory/secretreview/MainActivity.kt
+++ app/src/main/java/com/ffactory/secretreview/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.ffactory.secretreview
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.ffactory.secretreview.OptionPage.OptionPage
 import com.ffactory.secretreview.ReviewPage.ItemDataClass
 import com.ffactory.secretreview.ReviewPage.MainRecyclerViewAdapter
@@ -17,34 +19,54 @@ import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private var idItems = mutableListOf<String>()
+    private var loadItems = mutableListOf<ItemDataClass>()
+    private lateinit var adapter: MainRecyclerViewAdapter
+
     val db = Firebase.firestore
+
     @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val itemList = ArrayList<ItemDataClass>()
+        binding.RecyclerView.adapter = MainRecyclerViewAdapter(itemList)
+        binding.RecyclerView.layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)
+
+
         //DB에서 받아오기
-        db.collection("post")
-            .get()
-            .addOnSuccessListener {
-                Log.d("테스트","test")
+        loadItems.clear()
+        idItems.clear()
+        //adapter.clearItem()
+        val docRef = db.collection("post")
+        docRef.get()
+            .addOnSuccessListener { result ->
+                for(document in result){
+                Log.d("테스트",document.id)
+                    idItems.add(document.id)
+                    }
             }
+            .addOnFailureListener { exception ->
+                Log.d("테스트", "get failed with ", exception)
+            }
+
 
         //리사이클러뷰 부분
         //리사이클러뷰 테스트용 더미데이터
-        val recy = binding.RecyclerView
-        val itemList = ArrayList<ItemDataClass>()
-        itemList.add(ItemDataClass("1번 테스트","테스트 1-1", "1-2"))
-        itemList.add(ItemDataClass("2번 테스트","테스트 2-1", "2-2"))
-        itemList.add(ItemDataClass("3번 테스트","테스트 3-1", "3-2"))
-        itemList.add(ItemDataClass("4번 테스트","테스트 4-1", "4-2"))
-
-        val adapter = MainRecyclerViewAdapter(itemList)
-        adapter.notifyDataSetChanged()
-
-        recy.adapter = adapter
-        recy.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+//        val recy = binding.RecyclerView
+//        val itemList = ArrayList<ItemDataClass>()
+//        itemList.add(ItemDataClass("1번 테스트","테스트 1-1", "1-2"))
+//        itemList.add(ItemDataClass("2번 테스트","테스트 2-1", "2-2"))
+//        itemList.add(ItemDataClass("3번 테스트","테스트 3-1", "3-2"))
+//        itemList.add(ItemDataClass("4번 테스트","테스트 4-1", "4-2"))
+//
+//        val adapter = MainRecyclerViewAdapter(itemList)
+//        adapter.notifyDataSetChanged()
+//
+//        recy.adapter = adapter
+//        recy.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
         //더미데이터 끝
 
         //FAB 액티비티 전환부분
--- app/src/main/java/com/ffactory/secretreview/ReviewPage/MainRecyclerViewAdapter.kt
+++ app/src/main/java/com/ffactory/secretreview/ReviewPage/MainRecyclerViewAdapter.kt
@@ -7,34 +7,51 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.ffactory.secretreview.R
+import com.ffactory.secretreview.databinding.ReviewItemsBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
-class MainRecyclerViewAdapter(val itemList: ArrayList<ItemDataClass>) :
-    RecyclerView.Adapter<MainRecyclerViewAdapter.ViewHolder>() {
+class MainRecyclerViewAdapter(private val context: ArrayList<ItemDataClass>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     var review = mutableListOf<ItemDataClass>()
-    var documentID = mutableListOf<String>()
     val db = Firebase.firestore
-    var intent = Intent()
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.review_items, parent, false)
-        return ViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = ReviewItemsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return InnerClassHolder(view)
+    }
+    fun clearItem() {
+        review.clear()
+        notifyDataSetChanged()
+    }
+    override fun getItemCount() = review.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as InnerClassHolder).switch(position)
+    }
+    inner class InnerClassHolder(val binding: ReviewItemsBinding):
+        RecyclerView.ViewHolder(binding.root), View.OnClickListener{
+        var detailPage = binding.root
+        init {
+            detailPage.setOnClickListener(this)
+        }
+        fun switch(pos: Int){
+            val fullid = review[pos].name
+            binding.ItemName.text = review[pos].name
+            binding.ItemAddress.text = review[pos].add
+            binding.ItemTag.text = review[pos].tags
+        }
+
+        override fun onClick(v: View?) {
+            TODO("Not yet implemented")
+        }
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.tv_name.text = itemList[position].name
-        holder.tv_add.text = itemList[position].add
-        holder.tv_tags.text = itemList[position].tags
-    }
 
-    override fun getItemCount(): Int {
-        return itemList.count()
-    }
-
-    inner class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView){
-        val tv_name = itemView.findViewById<TextView>(R.id.ItemName)
-        val tv_add = itemView.findViewById<TextView>(R.id.ItemAddress)
-        val tv_tags = itemView.findViewById<TextView>(R.id.ItemTag)
-    }
+//      더미데이터용 뷰홀더
+//    inner class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView){
+//        val tv_name = itemView.findViewById<TextView>(R.id.ItemName)
+//        val tv_add = itemView.findViewById<TextView>(R.id.ItemAddress)
+//        val tv_tags = itemView.findViewById<TextView>(R.id.ItemTag)
+//    }
 }
