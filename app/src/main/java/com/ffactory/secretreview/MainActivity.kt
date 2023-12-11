d5ef702af70098c8b9868702ff3ead6b0d3b3c92
cbh1992 <whgg9786@gmail.com>
2023-12-11 14:33:43 +0900

    메인액티비티 및 어댑터 롤백

--- app/src/main/java/com/ffactory/secretreview/MainActivity.kt
+++ app/src/main/java/com/ffactory/secretreview/MainActivity.kt
@@ -1,47 +1,40 @@
 package com.ffactory.secretreview
 
 import android.annotation.SuppressLint
-import android.content.ContentValues
-import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.ffactory.secretreview.OptionPage.OptionPage
 import com.ffactory.secretreview.ReviewPage.ItemDataClass
+import com.ffactory.secretreview.ReviewPage.MainRecyclerViewAdapter
 import com.ffactory.secretreview.ReviewPage.NewReview
 import com.ffactory.secretreview.SearchPage.SearchPage
 import com.ffactory.secretreview.databinding.ActivityMainBinding
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.Query
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-
-//피그마 링크 : https://www.figma.com/file/efKZcR1tdaAokOwT6aZ5vC/secretreview?type=whiteboard&node-id=0-1&t=YEI08CCyD8qXPBR6-0
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
-    private var data = mutableMapOf<String, ItemDataClass>()
-    private lateinit var comm: ItemDataClass
-    //private lateinit var adapter: MainRecyclerViewAdapter
-    val db = Firebase.firestore
-    var firestore : FirebaseFirestore? = null
     @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        firestore = FirebaseFirestore.getInstance()
-        binding.RecyclerView.adapter = RecyclerViewAdapter()
-        binding.RecyclerView.layoutManager = LinearLayoutManager(this)
+        //리사이클러뷰 부분
+        //리사이클러뷰 테스트용 더미데이터
+        val recy = binding.RecyclerView
+        val itemList = ArrayList<ItemDataClass>()
+        itemList.add(ItemDataClass("1번 테스트","테스트 1-1", "1-2"))
+        itemList.add(ItemDataClass("2번 테스트","테스트 2-1", "2-2"))
+        itemList.add(ItemDataClass("3번 테스트","테스트 3-1", "3-2"))
+        itemList.add(ItemDataClass("4번 테스트","테스트 4-1", "4-2"))
 
+        val adapter = MainRecyclerViewAdapter(itemList)
+        adapter.notifyDataSetChanged()
+
+        recy.adapter = adapter
+        recy.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+        //더미데이터 끝
 
         //FAB 액티비티 전환부분
         val New = binding.NewButton
@@ -58,39 +51,6 @@ class MainActivity : AppCompatActivity() {
         Option.setOnClickListener {
             val option = Intent(this, OptionPage::class.java)
             startActivity(option)
-        }
-    }
-    @SuppressLint("NotifyDataSetChanged")
-    inner class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
-        var simplereview : ArrayList<ItemDataClass> = arrayListOf()
-        init {
-            db.collection("post")
-                .get()
-                .addOnSuccessListener { result ->
-                    for (document in result) {
-                        Log.d(TAG, "post ${document.id} => ${document.data}")
-                    }
-                }
-                .addOnFailureListener { exception ->
-                    Log.d(TAG, "Error getting documents: ", exception)
-                }
-            notifyDataSetChanged()
-        }
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-            var view = LayoutInflater.from(parent.context).inflate(R.layout.review_items, parent, false)
-            return ViewHolder(view)
-        }
-        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
-
-        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-            var viewHolder = (holder as ViewHolder).itemView
-
-            viewHolder.name.text = simplereview[position].name
-            viewHolder.phoneNumber.text = simplereview[position].add
-            viewHolder.phoneNumber.text = simplereview[position].tags
-        }
-        override fun getItemCount(): Int {
-            return simplereview.size
         }
     }
 }
--- app/src/main/java/com/ffactory/secretreview/ReviewPage/MainRecyclerViewAdapter.kt
+++ app/src/main/java/com/ffactory/secretreview/ReviewPage/MainRecyclerViewAdapter.kt
@@ -1,45 +1,39 @@
-//package com.ffactory.secretreview.ReviewPage
-//
-//import android.app.Person
-//import android.content.Context
-//import android.content.Intent
-//import android.os.Build
-//import android.view.LayoutInflater
-//import android.view.View
-//import android.view.ViewGroup
-//import android.widget.TextView
-//import androidx.annotation.RequiresApi
-//import androidx.recyclerview.widget.RecyclerView
-//import com.ffactory.secretreview.R
-//import com.ffactory.secretreview.ReviewPage.ItemDataClass
-//import com.ffactory.secretreview.databinding.ActivityNewReviewBinding
-//import com.ffactory.secretreview.databinding.ReviewItemsBinding
-//import com.google.firebase.firestore.FirebaseFirestore
-//
-//@RequiresApi(Build.VERSION_CODES.P)
-//class MainRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-//    var firestore : FirebaseFirestore? = null
-//    var review : ArrayList<ItemDataClass> = arrayListOf()
-//    init {
-//        firestore = FirebaseFirestore.getInstance()
-//        firestore?.collection("post")?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
-//            // ArrayList 비워줌
-//            review.clear()
-//
-//            for (snapshot in querySnapshot!!.documents) {
-//                var item = snapshot.toObject(Person::class.java)
-//                review.add(item!!)
-//    }
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-//        TODO("Not yet implemented")
-//    }
-//
-//    override fun getItemCount(): Int {
-//        TODO("Not yet implemented")
-//    }
-//
-//    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-//        TODO("Not yet implemented")
-//    }
-//
-//}
+package com.ffactory.secretreview.ReviewPage
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ffactory.secretreview.R
+import com.ffactory.secretreview.ReviewPage.ItemDataClass
+import com.ffactory.secretreview.databinding.ActivityNewReviewBinding
+import com.ffactory.secretreview.databinding.ReviewItemsBinding
+
+class MainRecyclerViewAdapter(val itemList: ArrayList<ItemDataClass>) :
+    RecyclerView.Adapter<MainRecyclerViewAdapter.ViewHolder>() {
+
+    var review = mutableListOf<ItemDataClass>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.review_items, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.tv_name.text = itemList[position].name
+        holder.tv_add.text = itemList[position].add
+        holder.tv_tags.text = itemList[position].tags
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.count()
+    }
+
+    inner class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView){
+        val tv_name = itemView.findViewById<TextView>(R.id.ItemName)
+        val tv_add = itemView.findViewById<TextView>(R.id.ItemAddress)
+        val tv_tags = itemView.findViewById<TextView>(R.id.ItemTag)
+    }
+}
